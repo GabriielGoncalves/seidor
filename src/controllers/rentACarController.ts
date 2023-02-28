63ef86c5d8c29c1bc2a44b5c1ff8821ee3411034
GabriielGoncalves <novais_gabriel@outlook.com>
2023-02-28 04:06:28 -0300

    refactor: :art: remove logic from the 'controller' layer in the 'rents' service and create the service layer to perform the processing

--- src/controllers/rentACarController.ts
+++ src/controllers/rentACarController.ts
@@ -1,42 +1,24 @@
 import { Request, Response } from 'express';
-import Database from '../models/database';
-
+import RentACarService from '../services/RentACarService';
 class RentACar {
     constructor() {}
 
     async rentACar(req: Request, res: Response) {
-        try {
-            const { driver, car } = req.params;
-            const description: string = req.body.description;
+        const { driver, car } = req.params;
+        const description: string = req.body.description;
 
-            const db = Database.createDb();
-            const result = await db.registerRent(driver, car, description);
+        await RentACarService.register(driver, car, description);
 
-            if (!result) {
-                return res.status(201).json({
-                    msg: 'Rental registration completed successfully',
-                });
-            }
-
-            return res.status(400).json({
-                msg: 'Oops. The desired car is already rented. We are sorry.',
-            });
-        } catch (error) {
-            throw new Error((error as Error).message);
-        }
+        return res.status(201).json({
+            msg: 'Rental registration completed successfully',
+        });
     }
 
     async listAllRents(req: Request, res: Response) {
-        try {
-            const db = Database.createDb();
-            const result = await db.findRents();
-
-            return res.status(200).json({
-                msg: result,
-            });
-        } catch (error) {
-            throw new Error((error as Error).message);
-        }
+        const result = await RentACarService.find();
+        return res.status(200).json({
+            msg: result,
+        });
     }
 
     teste() {}
---
+++ src/services/RentACarService.ts
@@ -0,0 +1,70 @@
+import { RentACar } from '../models/entity/RentACar';
+import AppDataSource from '../models/data-source/data-source';
+import CarService from './CarService';
+import DriverService from './DriverService';
+
+class RentACarService {
+    private readonly rentACarRepository = AppDataSource.getRepository(RentACar);
+    private readonly carService = CarService;
+    private readonly driverService = DriverService;
+
+    async register(
+        driver: string,
+        car: string,
+        description: string,
+    ): Promise<RentACar> {
+        const rent = await this.findRentByDriver(driver);
+        const getCar = await this.carService.findById(car);
+        const getDriver = await this.driverService.findById(driver);
+
+        if (rent?.car.id === getCar.id) {
+            throw new Error(
+                'Oops. The desired car is already rented. We are sorry',
+            );
+        }
+
+        if (rent) {
+            throw new Error(
+                'Oops. The driver cannot rent another car at this time. End your contract and try again.',
+            );
+        }
+
+        const newRent = this.rentACarRepository.create({
+            description,
+            driver: getDriver,
+            car: getCar,
+        });
+
+        const register = await this.rentACarRepository.save(newRent);
+        return register;
+    }
+
+    private async findRentByDriver(idDriver: string): Promise<RentACar | null> {
+        const rent = await this.rentACarRepository.findOne({
+            where: {
+                driver: {
+                    id: idDriver,
+                },
+            },
+            relations: {
+                car: true,
+                driver: true,
+            },
+        });
+
+        return rent ? rent : null;
+    }
+
+    async find(): Promise<RentACar[] | string> {
+        const rents = await this.rentACarRepository.find({
+            relations: {
+                car: true,
+                driver: true,
+            },
+        });
+
+        return rents.length === 0 ? 'there are no rentals' : rents;
+    }
+}
+
+export default new RentACarService();
